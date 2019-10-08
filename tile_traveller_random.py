c10303c9d1924a762b073bfd2c066f60c9bf1464
Nanna Katrín Hannesdóttir <Nanna@Nannas-MacBook-Air-4.local>
2019-10-08 10:57:11 +0000

    random tile t

--- tile_traveller_random.py
+++ tile_traveller_random.py
@@ -1,4 +1,4 @@
-#print ("Yibby   eh er hewr hrewh rewh hrew  q!")
+import random
 
 #skref1
 """búa til function move sem tekur inn staðsetningu og beiðni um færslu og skilar
@@ -25,9 +25,10 @@ def move_val(location,direction):
         location -= 1.0
         return location
 
-def pull_a_lever(total_coin):
+def pull_a_lever(total_coin, y_n):
 
-    pull = input("Pull a lever (y/n): ").lower()
+    pull = random.choice(y_n)
+    print("Pull a lever (y/n):", pull)
     
     if pull == "y":
         total_coin += 1
@@ -39,104 +40,128 @@ def pull_a_lever(total_coin):
     return total_coin
 
 
-def move(location):
+def move(location, directions):
     """ takes in a location and returns a new location by conditions"""
     if location == 1.1:
         print("You can travel: (N)orth.")
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while direction != NORTH:
             print("Not a valid direction!")
             print("You can travel: (N)orth.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
     elif location == 1.2:
         print("You can travel: (N)orth or (E)ast or (S)outh.") 
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while direction!= NORTH and direction!= EAST and direction != SOUTH:
             print("Not a valid direction!")
             print("You can travel: (N)orth or (E)ast or (S)outh.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
     elif location == 1.3:
         print("You can travel: (E)ast or (S)outh.")
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while  direction!= EAST and direction!= SOUTH:
             print("Not a valid direction!")
             print("You can travel: (E)ast or (S)outh.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
     elif location == 2.1:
         print("You can travel: (N)orth.")
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while direction != NORTH:
             print("Not a valid direction!")
             print("You can travel: (N)orth.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
     elif location == 2.2:
         print("You can travel: (S)outh or (W)est.")
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while direction != SOUTH and direction != WEST:
             print("Not a valid direction!")
             print("You can travel: (S)outh or (W)est.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
     elif location == 2.3:
         print("You can travel: (E)ast or (W)est.")
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while direction != EAST and direction != WEST:
             print("Not a valid direction!")
             print("You can travel: (E)ast or (W)est.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
     elif location == 3.2:
         print("You can travel: (N)orth or (S)outh.")
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while direction != NORTH and direction != SOUTH:
             print("Not a valid direction!")
             print("You can travel: (N)orth or (S)outh.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
     elif location == 3.3:
         print("You can travel: (S)outh or (W)est.")
-        direction = input("Direction: ").lower()
+        direction = random.choice(directions)
+        print("Direction:", direction)
         while direction != SOUTH and direction != WEST:
             print("Not a valid direction!")
             print("You can travel: (S)outh or (W)est.")
-            direction = input("Direction: ").lower()
+            direction = random.choice(directions)
+            print("Direction:", direction)
         else:
             location = move_val(location,direction)
             return round(location,1)
 
 
-def the_game(position, total_coin, lever_position):
+def the_game(position, total_coin, lever_position, directions,y_n):
+    input_seed = int(input("Input seed: "))
+    random.seed(input_seed)
+    move_count = 0
     while position != 3.1:
-        position = move(position)
+        position = move(position, directions)
         if position in lever_position:
-            total_coin = pull_a_lever(total_coin)
-    print("Victory! Total coins {}.".format(total_coin))
+            total_coin = pull_a_lever(total_coin,y_n)
+        move_count +=1
+    print("Victory! Total coins {}. Moves {}.".format(total_coin,move_count))
 
 position = 1.1
 total_coin = 0
 lever_position = [1.2 , 2.2 , 2.3 , 3.2]
+directions = [NORTH,EAST,SOUTH, WEST]
+y_n = ["y","n"]
 
-the_game(position, total_coin, lever_position)
+the_game(position, total_coin, lever_position, directions,y_n)
 play_again = input("Play again (y/n): ").lower()
 
 while play_again == "y":
-    the_game(position, total_coin, lever_position)
+    the_game(position, total_coin, lever_position,directions,y_n)
     play_again = input("Play again (y/n): ").lower()
+
+
